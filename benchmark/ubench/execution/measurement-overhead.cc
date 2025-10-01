b072e1b7d4bf3aa007015aa8210a6b985af3adcc
studyztp <studyztp@gmail.com>
2025-10-01 13:32:46 -0400

    add measurment overhead ubench

---
+++ benchmark/ubench/execution/measurement-overhead.cc
@@ -0,0 +1,25 @@
+#include "common.h"
+
+extern "C" {
+
+// This macro inserts 8 NOP instructions to match the instruction count.
+// Each NOP takes 1 cycle on STM32-G4 so the expected total cycles is:
+// Measured_Cycles = ARRAY_SIZE × (8). 
+// Any extra cycles measured is the overhead of measurement itself.
+
+#define JUST_NOPS \
+    asm volatile(                     \
+        ".p2align 4             \n"   \
+        PAYLOAD_NOPS(8)               \
+        ::: "memory");      
+
+__attribute__ ((noinline))
+void init(uint32_t *array, const size_t size, const size_t stride) {
+    // do nothing
+}
+
+__attribute__ ((noinline))
+void ubench(uint32_t *array, const size_t size) {
+    REPEAT_N(ARRAY_SIZE, JUST_NOPS);
+}
+}
